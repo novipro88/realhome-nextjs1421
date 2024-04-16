330eb2cf3bf4067d932d347e253dca701f6f11ad
novipro88 <noviantipali97@gmail.com>
2024-04-17 02:17:43 +0700

    initial commit

---
+++ src/app/(root)/layout.js
@@ -0,0 +1,13 @@
+import Header from "@/components/Header";
+import { Provider } from "@/providers/provider";
+
+export default function RootLayout({ children }) {
+  return (
+    <main>
+      <Provider>
+        <Header />
+        {children}
+      </Provider>
+    </main>
+  );
+}
---
+++ src/components/Divider.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Divider = () => {
+  return <div className="bg-slate-200 p-[0.5px] my-3"></div>;
+};
+
+export default Divider;
